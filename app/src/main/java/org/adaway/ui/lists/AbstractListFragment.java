058ec134ffa31f5b85139c5729123a87d0a59588
Harsh Shandilya <msfjarvis@gmail.com>
2019-07-14 17:54:40 +0530

    Fix abuse of this#member
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/org/adaway/ui/lists/AbstractListFragment.java
+++ app/src/main/java/org/adaway/ui/lists/AbstractListFragment.java
@@ -63,7 +63,7 @@ public abstract class AbstractListFragment extends Fragment implements ListsView
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Store activity
-        this.mActivity = this.getActivity();
+        mActivity = getActivity();
         // Create fragment view
         View view = inflater.inflate(R.layout.hosts_lists_fragment, container, false);
         /*
@@ -73,16 +73,16 @@ public abstract class AbstractListFragment extends Fragment implements ListsView
         RecyclerView recyclerView = view.findViewById(R.id.hosts_lists_list);
         recyclerView.setHasFixedSize(true);
         // Defile recycler layout
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.mActivity);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mActivity);
         recyclerView.setLayoutManager(linearLayoutManager);
         // Create recycler adapter
-        ListAdapter adapter = new ListsAdapter(this, this.isTwoRowsItem());
+        ListAdapter adapter = new ListsAdapter(this, isTwoRowsItem());
         recyclerView.setAdapter(adapter);
         /*
          * Create action mode.
          */
         // Create action mode callback to display edit/delete menu
-        this.mActionCallback = new ActionMode.Callback() {
+        mActionCallback = new ActionMode.Callback() {
             @Override
             public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
                 // Get menu inflater
@@ -104,22 +104,22 @@ public abstract class AbstractListFragment extends Fragment implements ListsView
             @Override
             public boolean onActionItemClicked(ActionMode actionMode, MenuItem item) {
                 // Check action item
-                if (AbstractListFragment.this.mActionItem == null) {
+                if (mActionItem == null) {
                     return false;
                 }
                 // Check item identifier
                 switch (item.getItemId()) {
                     case R.id.checkbox_list_context_edit:
                         // Edit action item
-                        AbstractListFragment.this.editItem(AbstractListFragment.this.mActionItem);
+                        editItem(mActionItem);
                         // Finish action mode
-                        AbstractListFragment.this.mActionMode.finish();
+                        mActionMode.finish();
                         return true;
                     case R.id.checkbox_list_context_delete:
                         // Delete action item
-                        AbstractListFragment.this.deleteItem(AbstractListFragment.this.mActionItem);
+                        deleteItem(mActionItem);
                         // Finish action mode
-                        AbstractListFragment.this.mActionMode.finish();
+                        mActionMode.finish();
                         return true;
                     default:
                         return false;
@@ -129,22 +129,22 @@ public abstract class AbstractListFragment extends Fragment implements ListsView
             @Override
             public void onDestroyActionMode(ActionMode actionMode) {
                 // Clear view background color
-                if (AbstractListFragment.this.mActionSourceView != null) {
-                    AbstractListFragment.this.mActionSourceView.setBackgroundColor(Color.TRANSPARENT);
+                if (mActionSourceView != null) {
+                    mActionSourceView.setBackgroundColor(Color.TRANSPARENT);
                 }
                 // Clear current source and its view
-                AbstractListFragment.this.mActionItem = null;
-                AbstractListFragment.this.mActionSourceView = null;
+                mActionItem = null;
+                mActionSourceView = null;
                 // Clear action mode
-                AbstractListFragment.this.mActionMode = null;
+                mActionMode = null;
             }
         };
         /*
          * Load data.
          */
         // Get view model and bind it to the list view
-        this.mViewModel = ViewModelProviders.of(this).get(ListsViewModel.class);
-        this.getData().observe(this, adapter::submitList);
+        mViewModel = ViewModelProviders.of(this).get(ListsViewModel.class);
+        getData().observe(this, adapter::submitList);
         // Return created view
         return view;
     }
@@ -152,18 +152,18 @@ public abstract class AbstractListFragment extends Fragment implements ListsView
     @Override
     public boolean startAction(HostListItem item, View sourceView) {
         // Check if there is already a current action
-        if (this.mActionMode != null) {
+        if (mActionMode != null) {
             return false;
         }
         // Store current source and its view
-        this.mActionItem = item;
-        this.mActionSourceView = sourceView;
+        mActionItem = item;
+        mActionSourceView = sourceView;
         // Get current item background color
-        int currentItemBackgroundColor = this.getResources().getColor(R.color.selected_background);
+        int currentItemBackgroundColor = getResources().getColor(R.color.selected_background);
         // Apply background color to view
-        this.mActionSourceView.setBackgroundColor(currentItemBackgroundColor);
+        mActionSourceView.setBackgroundColor(currentItemBackgroundColor);
         // Start action mode and store it
-        this.mActionMode = this.mActivity.startActionMode(this.mActionCallback);
+        mActionMode = mActivity.startActionMode(mActionCallback);
         // Return event consumed
         return true;
     }
@@ -172,8 +172,8 @@ public abstract class AbstractListFragment extends Fragment implements ListsView
      * Ensure action mode is cancelled.
      */
     void ensureActionModeCanceled() {
-        if (this.mActionMode != null) {
-            this.mActionMode.finish();
+        if (mActionMode != null) {
+            mActionMode.finish();
         }
     }
 
@@ -188,11 +188,11 @@ public abstract class AbstractListFragment extends Fragment implements ListsView
     protected abstract void editItem(HostListItem item);
 
     protected void deleteItem(HostListItem item) {
-        this.mViewModel.removeListItem(item);
+        mViewModel.removeListItem(item);
     }
 
     @Override
     public void toggleItemEnabled(HostListItem list) {
-        this.mViewModel.toggleItemEnabled(list);
+        mViewModel.toggleItemEnabled(list);
     }
 }
